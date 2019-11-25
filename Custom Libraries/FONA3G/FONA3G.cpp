0dc5ca2822c1d87cd093c22e0bdf76973d338d40
Hawes Collier <37671636+hawes1010@users.noreply.github.com>
2019-11-25 17:00:31 -0500

    Create FONA3G.cpp

---
+++ Custom Libraries/FONA3G/FONA3G.cpp
@@ -0,0 +1,46 @@
+//Fona 3G Libray
+//Hawes Collier with quotes from Arduino's FONAtest
+#include "FONA3G.h"
+
+
+//CONSTRUCTOR
+//FONA3G will use functions that simplify HTTP Requests 
+//
+//
+
+FONA3G::FONA3G(){
+	
+	
+}
+
+void FONA3G::PostRequest(void) {
+	
+	
+}
+
+void FONA3G::ReadSMS(void) {
+	
+	
+}
+
+void FONA3G::DeleteSMS(void) {
+	
+	
+}
+
+void FONA3G::sendSMS(void) {
+	
+	
+}
+
+void FONA3G::ReadSMS(void) {
+	
+	
+}
+
+
+
+
+
+
+
