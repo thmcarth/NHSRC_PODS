6b4848fa0c3169bf08481d0b8e37c27756e02b87
Hawes Collier <37671636+hawes1010@users.noreply.github.com>
2020-02-20 08:49:46 -0500

    Update Rain_Sensor_Module.ino

--- Rain Sensor Module/Rain_Sensor_Module/Rain_Sensor_Module.ino
+++ Rain Sensor Module/Rain_Sensor_Module/Rain_Sensor_Module.ino
@@ -10,12 +10,19 @@ Serial.begin(9600);
 
 void loop() {
   // put your main code here, to run repeatedly:
-if (analogRead(analog_line) <300)
-condition = 3;
-if (analogRead(analog_line) <500)
-condition = 2;
-else
-condition = 1;
+if (analogRead(analog_line) <300){
+condition = 3; // lots 
+Serial.println("Lots of rain");
+}
+else if (analogRead(analog_line) <500){
+condition = 2;  // some
+Serial.println("some of rain");
 
+}
+else{
+condition = 1; //not much
+Serial.println("not rain");
+
+}
 delay(250); // remove this if the loop becomes a function in a separate program
 }
