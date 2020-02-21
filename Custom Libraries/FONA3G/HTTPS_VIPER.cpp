b193ec32b8e491ff95ed0b726815feb57fecf809
Hawes Collier <37671636+hawes1010@users.noreply.github.com>
2020-02-21 15:07:18 -0500

    Developing HTTP Library

--- Custom Libraries/FONA3G/HTTPS_VIPER.cpp
+++ Custom Libraries/FONA3G/HTTPS_VIPER.cpp
@@ -18,7 +18,7 @@
 
 HTTPS_VIPER::HTTPS_VIPER()
 {
-  
+  Adafruit_FONA_3G fona = Adafruit_FONA_3G(FONA_RST)
 }
 
 int HTTPS_VIPER::getLength(char* content){
@@ -26,7 +26,7 @@ int values = sizeof(content);
 return values;
 }
 
-char* HTTPS_VIPER::buildHTTP_POST(int data_length, char* host,char* auth){
+    char* HTTPS_VIPER::build_POST(int data_length, char* host,char* auth, char* body){
 	char* header;
 	char post[] = "POST /CAP/post HTTP/1.1\n";  // Note! Order does not matter for an http Header
     char connection[] = "Connection: Keep-Alive\n";
@@ -37,5 +37,78 @@ char* HTTPS_VIPER::buildHTTP_POST(int data_length, char* host,char* auth){
 	char *content;
 	sprintf(content, "Content-Length: %s\n", data_length);
 	sprintf(header,"%s%s%s%s%s", post, host, authorization,content, connection);
+	char* full_message
+	sprintf(full_message,
+
 }
+
+void HTTPS_VIPER::start_HTTP(Serial s){
+	// turn HTTPS Stack on
+  // how do i send serial and AT commands?
+  s.println("AT+CHTTPSSTART");
+	
+}
+
+void HTTPS_VIPER::Open_HTTP(Serial s, char* host, char* port){
+char* open;
+sprintf(open, "AT+CHTTPSOPSE=\"%s\",%s,2", host, port);
+s.println(open);
+}
+
+void HTTPS_VIPER::Send_HTTP(Serial s, int length, char* post){
+	char* send;
+	int ready = 0;
+	sprintf(send, "AT+CHTTPSSEND=%i", length);
+	s.println(send);
+	delay(10);
+	 if (Serial.available() > 0) {
+    incomingByte = s.read();
+	for(int i = 0; i<100; i++){
+	if (incomingByte == 62) // we are waiting for the > symbol from the FONA 
+	ready = 1;
+    delay (10);
+	}
+	if (ready){
+	s.print(post);	
+	}
+	 }
+}
+
+void HTTPS_VIPER::Stop_HTTP(Serial s){
+s.println("AT+CHTTPSSTOP");
+}
+
+void HTTPS_VIPER::Close_HTTP(Serial s){
+	s.println("AT+CHTTPSCLSE");
+}
+
+
+/*
+ Serial.println(AT+CGREG);
+
+  // Check Packet Domain attach or detach
+  Serial.println(AT+CGATT?);
+
+  //Set the PDP Contect Profile
+  Serial.println(AT+CGSOCKCONT=1,"IP","live.vodafone.com");
+
+  Serial.println(AT+CGDCONT=1,"IP","live.vodafone.com","0.0.0.0",0,0);
+  Serial.println(AT+CGAUTH=1,1,"","");
+  Serial.println(AT+CGATT=1 );
+
+  // Activate or deactivate the specified PDP contect(s)
+  Serial.println(AT+CGACT=1,1 );
+  Serial.println(AT+CSOCKSETPN=1 );
+
+  // Check network registration status
+  Serial.println(AT+CGREG);
+
+  // Check Packet Domain attach or detach
+  Serial.println(AT+CGATT?);
+
+
+   getHTMLPage();
+
+*/
+
 
