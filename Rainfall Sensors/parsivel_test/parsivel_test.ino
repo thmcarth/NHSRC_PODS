47367ffd909b136649ab74c20b2935b5775b276f
Hawes Collier <37671636+hawes1010@users.noreply.github.com>
2020-11-10 07:30:25 -0500

    Update parsivel_test.ino

--- Rainfall Sensors/parsivel_test/parsivel_test.ino
+++ Rainfall Sensors/parsivel_test/parsivel_test.ino
@@ -1,89 +1,65 @@
+/*
+ * RS485 Master Software
+ * Exercise the MAX485 Module.  This code runs on the first (Master)device.
+ * Use the RS485_Slave_Test software for the second (Slave) device
+ * This uses the SoftSerial.h library which comes with the Arduino IDE
+ * Pins used for the soft serial port are fairly arbitrary and can be changed
+ * as needed.  Just redefine them below.
+ */
+ #define parsivelSerial Serial1
+#include <SoftwareSerial.h>
+const int SSERIAL_RX_PIN = 0;  //Soft Serial Receive pin
+const int SSERIAL_TX_PIN = 1;  //Soft Serial Transmit pin
+const int SSERIAL_CTRL_PIN= 8;   //RS485 Direction control
+//const int LED_PIN = 13;
+const int RS485_TRANSMIT = HIGH;
+const int RS485_RECEIVE = LOW;
 
-/////////////////////////Parsivel data 
-String parsivel_data;
-char* parsivel_intensity;
-/////////////////////////
-void setup() {
-  // put your setup code here, to run once:
-Serial2.begin(19200);
-Serial.begin(9600);
-delay(5000);
-//setup_parsivel();
-}
+// Create Soft Serial Port object and define pins to use
+SoftwareSerial RS485Serial(SSERIAL_RX_PIN, SSERIAL_TX_PIN); // RX, TX
 
-void loop() {
-  delay(1000);
-  //read_parsivel();
-  if (Serial2.available()>0)
-  Serial.println(Serial2.readString());
-  else 
-  Serial.println("sad");
-}
 
-void setup_parsivel() { // Tells the Parsivel through serial message how we want to get the telegram data from it
-  //refer to OneDrive in Parsivel2-->Terminal Commands Pdf.
-  String interval = "10"; // This is how often we want to receive data from the parsivel
+String byteReceived;
+//===============================================================================
+//  Initialization
+//===============================================================================
+void setup()
+{
+  delay(200);
+  Serial.begin(9600);           // Start the built-in serial port
+  Serial.println("Master Device");
+  Serial.println("Type in upper window, press ENTER");
   
-  //String request_data = "CS/M/S/%01,/%02,/%60,/%34,/%18,/%93/r/n/%61/r/\r";// this asks for  intensity, rain accumulated, particles detected, 
-  // kinetic energy, and raw data (in this order)
-  String request_data = "CS/M/S/%01,%02,%60,%34,%18,%93/r/n";
-  String interval_send = "CS/I/10";
-  String enable_msg = "CS/M/M/1";
-  Serial2.print(request_data);
-  delay(50);
-  Serial.println(Serial2.readString());
-  Serial.println(Serial2.readString());
-  Serial2.print(interval_send);
-  Serial.println(Serial2.readString());
+ // pinMode(LED_PIN, OUTPUT);     // Configure any output pins
+  pinMode(SSERIAL_CTRL_PIN, OUTPUT);    
+  delay(3);
+  digitalWrite(SSERIAL_CTRL_PIN, RS485_RECEIVE);  // Put RS485 in receive mode  
   
-  Serial.println("Parsivel Setup");
+  RS485Serial.begin(9600);   // Start the RS485 soft serial port 
 }
-
-void read_parsivel(){
-  Serial.println("Start Reading");
-  String message = "";
-  int i = 0;
-  //Serial2.print("CS/R/%13\r");
-  delay(500);
-  if (Serial2.available()>0){
-   message = Serial2.readString();
+//===============================================================================
+//  Main
+//===============================================================================
+void loop() 
+{
+  if (Serial.available())         // A char(byte) has been entered in the Serial Monitor
+  {
+    Serial.print("Sending: ");
+    byteReceived = Serial.readString();                   // Read the byte
+    Serial.println(byteReceived);
+    digitalWrite(SSERIAL_CTRL_PIN, RS485_TRANSMIT);  // Put RS485 in Transmit mode   
+    delay(1);          
+    RS485Serial.print(byteReceived);                 // Send byte to Remote Arduino
+    delay(1);                                        // Wait before going back to Receive mode
+    digitalWrite(SSERIAL_CTRL_PIN, RS485_RECEIVE);   // Put RS485 back into Receive mode    
   }
-    Serial.print("Done Reading, msg is: ");
-  Serial.println(message);
-  parsivel_data = message;
-  parsivel_intensity = parse_Intensity(message);
-  Serial.print("Done Reading, value is: ");
-  Serial.println(parsivel_intensity);
-}
-
-char* parse_Intensity(String message){
-Serial.print("Message is");
-Serial.println(message);
-int len = message.length();
-int indx_semi_1 = 0;
-int indx_semi_2 = 0;
-int i = 0;
-int count = 0;
-String mess;
-char* intensity;
-if (len >= 2){  // Intensity value is at least 8 digits.  If the message isnt this long, there's nothing to look at.
-for ( i = 0; i < len; i++)
-    if (message.charAt(i) == ';')
-       count++;
-    if (count == 1)
-      indx_semi_1 = i;
-    if (count == 2){
-      indx_semi_2 = i;
-      i = len;
-    }
- mess = message.substring(indx_semi_1, indx_semi_2);
- int leng = mess.length();
- mess.toCharArray(intensity,leng+1);
- return intensity;
-}
-else
-return "Empty";
-}
-void serialEvent2() {
-  Serial.println(Serial2.readString());
+  
+  if (RS485Serial.available())            //Data from the Slave is available
+   {
+     digitalWrite(SSERIAL_CTRL_PIN, RS485_RECEIVE);
+     delay(10);
+    byteReceived = RS485Serial.read();    // Read received byte
+    Serial.print(byteReceived);           // Show on Serial Monitor
+   }  
+   delay(1);
 }
